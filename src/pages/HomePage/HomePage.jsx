21ad205c0a371c7add8d407f72cf3e76aa76e63b
Mikita Kamiakevich <79355999+nikitsch@users.noreply.github.com>
2023-07-04 19:42:46 +0300

    feat/HomePage (#3)
    * feat: container HomePage
    
    * faet: card products

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,14 +1,18 @@
 import { useSearchProductsQuery } from "../../store/shopAPI/shop.api"
+import { Card } from "./Card/Card"
 import style from "./HomePage.module.css"
 
 export function HomePage() {
-  const {isLoading, isError, data} = useSearchProductsQuery()
-  console.log(data);
+  const { isLoading, isError, data: products } = useSearchProductsQuery()
+  // console.log(products.map(i => i.title));
   return (
     <>
-    {isError && <p className={style.error}>Something went wrong...</p>}
-    {isLoading && <p className={style.loading}>Loading...</p>}
-    <div>Home</div>
+      {isError && <p className={style.error}>Something went wrong...</p>}
+      {isLoading && <p className={style.loading}>Loading...</p>}
+
+      <div className={style.container}>
+        {products?.map(item => <Card item={item} key={item.id} />)}
+      </div>
     </>
   )
 }
