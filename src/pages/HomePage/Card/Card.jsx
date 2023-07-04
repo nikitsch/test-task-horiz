dffd38f797729af2d0d9714472f3ab51936ff08f
nikitsch <nikitsch5@gmail.com>
2023-07-04 19:42:10 +0300

    faet: card products

--- src/pages/HomePage/Card/Card.jsx
+++ src/pages/HomePage/Card/Card.jsx
@@ -1,7 +1,11 @@
+import style from "./Card.module.css"
 
-
-export function Card() {
+export function Card({item}) {
   return (
-    <div>Card</div>
+    <div className={style.card}>
+      <img className={style.image} src={item.image} alt="" />
+      <h3 className={style.title}>{item.title}</h3>
+      {/* <h2 className={style.price}>{item.price} BYN</h2> */}
+    </div>
   )
 }
