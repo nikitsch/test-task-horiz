cc3a0b067921dc5e56833cea061699b2c97a8e30
nikitsch <nikitsch5@gmail.com>
2023-07-05 22:43:57 +0300

    feat: filter logic

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import { useSearchProductsQuery } from "../../store/shopAPI/shop.api"
 import { Card } from "./Card/Card"
 import style from "./HomePage.module.css"
@@ -5,13 +6,35 @@ import style from "./HomePage.module.css"
 export function HomePage() {
   const { isLoading, isError, data: products } = useSearchProductsQuery()
 
+  const [filter, setFilter] = useState("all")
+
+  const setToFilter = (event) => {
+    event.preventDefault()
+    setFilter(event.target.value)
+  }
+
+  const lists = [...new Set(products?.map(item => item.category))]
+
   return (
     <>
       {isError && <p className={style.error}>Something went wrong...</p>}
-      {isLoading && <p className={style.loading}>Loading...</p>}
+      
+      {isLoading
+        ? <p className={style.loading}>Loading...</p>
+
+        : <div className={style.filter}>
+          <select onChange={setToFilter}>
+            <option>all</option>
+            {lists.map(list => <option value={list}>{list}</option>)}
+          </select>
+        </div>
+      }
 
       <div className={style.container}>
-        {products?.map(item => <Card product={item} key={item.id} />)}
+        {products?.map(item => {
+          if (filter === "all") return <Card product={item} key={item.id} />
+          if (item.category === filter) return <Card product={item} key={item.id} />
+        })}
       </div>
     </>
   )
